10fddba6dd15f1f02ad5aacfd9be0a7e08f7ec5d
rishabh821 <r.singh100101@gmail.com>
2026-03-23 21:36:20 +0530

    first commit

---
+++ Bus_tracker_try/src/main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+bus = {
+    "lat": 28.6139,
+    "lng": 77.2090
+}
+
+@app.get("/")
+def home():
+    return {"message": "NEW VERSION OF BUS TRACKER IS LIVE! CHECK OUT THE NEW FEATURES!"}
+
+@app.get("/location")
+def get_location():
+    return bus
+
+@app.post("/update")
+def update_location(lat: float, lng: float):
+    global bus
+    bus = {"lat": lat, "lng": lng}
+    return {"status": "updated"}
+
+@app.get("/driver")
+def driver():
+    return FileResponse("driver.html")
